b29f79265497481c94bbacd0895ca6107c1f294e
Yuran Saraiva <yrnsaraiva03@gmail.com>
2026-06-19 21:05:59 +0200

    updates

--- config/settings.py
+++ config/settings.py
@@ -333,11 +333,11 @@ SOCIALACCOUNT_PROVIDERS = {
 # Para desenvolvimento — imprime o email na consola
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.us.appsuite.cloud'
-EMAIL_PORT = 465
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'geral@zonal.co.mz'
-EMAIL_HOST_PASSWORD = 'za4798wg5'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'zonal <no-reply@zonal.co.mz>'
 FRONTEND_URL = 'https://zonal.co.mz'
