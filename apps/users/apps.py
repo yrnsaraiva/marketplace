6287c57173a0f5da793bb61248d5fb70cc43448b
Yuran Saraiva <yrnsaraiva03@gmail.com>
2026-06-20 08:33:00 +0200

    updates

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'apps.users'
+
+    def ready(self):
+        # Registar os signal handlers definidos em views.py com @receiver
+        import apps.users.views  # noqa: F401
