734eac5cfb1f7c697ecc2527fd580f76490d57bb
Yuran Saraiva <yrnsaraiva03@gmail.com>
2026-06-20 08:03:31 +0200

    TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app',
     'http://localhost',
     'http://127.0.0.1',
-    'www.zonal.co.mz',
-    'zonal.co.mz'
+    'https://www.zonal.co.mz',
+    'https://zonal.co.mz'
 ]
 
 # Application definition
